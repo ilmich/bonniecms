598c33d426cfe23cbcd99c3aeb3cfa7c561cdd66
ilmich <ardutu@gmail.com>
2010-10-28 01:02:42 +0200

    Added sample localization

--- data/menus.php
+++ data/menus.php
@@ -2,7 +2,7 @@
 
 	$menu_list = array(
 		"top_menu" => array(
-					"home" => array(makeLink("home"),"Home")					
-					,"getstarted" => array(makeLink("getstarted"),"Get Started")
+					"home" => array(makeLink("home"),Lang::getMessage("Home"))					
+					,"getstarted" => array(makeLink("getstarted"),Lang::getMessage("Get Started"))
 					)
 	);
