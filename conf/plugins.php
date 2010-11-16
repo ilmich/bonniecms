040e0b976b3a059e8b8211698c4d38c160e968fd
ilmich <ardutu@gmail.com>
2010-11-16 21:45:40 +0100

    Added simple profiler plugin, and weblog plugin

--- conf/plugins.php
+++ conf/plugins.php
@@ -1,4 +1,6 @@
 <?php
-
-	return array("compress",				
-				"zcache");
+	
+	return array(//"weblog", //uncoment this to enable weblog plugin
+				//"profiler", //uncoment this to enable simple profiler
+				"compress",				
+				"zcache" //ATTENTION!!! the cache plugin must be the last);
---
+++ plugins/profiler.php
@@ -0,0 +1,32 @@
+<?php if (!defined('CLYDEPHP')) die("Direct access not allowed") ;
+
+	$startTime=0;
+	$memStart=0;
+	
+	EventManager::getInstance()->getEvent("processRequest")->subscribe("startProfiler");
+	EventManager::getInstance()->getEvent("processResponse")->subscribe("endProfiler");
+	
+	
+	function startProfiler($res) {
+		
+		global $startTime,$memStart;
+		
+		$startTime = microtime(true);
+		$memStart = memory_get_usage();
+		
+	}
+	
+	function endProfiler($req,$resp) {
+		
+		global $startTime,$memStart;
+		
+		$timeElapsed = (float)(microtime(true)-$startTime);
+		$memUsage = (float)(memory_get_usage() - $memStart);
+		
+		$body = $resp->getBody();		
+		$body.="<center>Time elapsed ".$timeElapsed." | Memory usage ".bytes2str($memUsage)."</center>";
+		
+		$resp->setBody($body);
+		
+	}
+
