8aaac547189655775da63a3398aa9924988573ae
ilmich <ardutu@gmail.com>
2011-02-24 17:56:12 +0100

    Removed zcache plugin

--- conf/plugins.php
+++ conf/plugins.php
@@ -1,6 +1,8 @@
 <?php
 	
 	return array(//"weblog", //uncoment this to enable weblog plugin
-				//"profiler", //uncoment this to enable simple profiler
-				"compress",				
-				"zcache"); //ATTENTION!!! the cache plugin must be the last
+				"profiler", //uncoment this to enable simple profiler
+				//"minify",
+				//"compress"
+				);				
+				
