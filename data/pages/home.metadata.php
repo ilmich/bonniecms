2a3bafcc478f4ea293276158be31f33854ecb161
ilmich <ardutu@gmail.com>
2011-02-24 15:48:26 +0100

    Sample meta tags

--- data/pages/home.metadata.php
+++ data/pages/home.metadata.php
@@ -2,5 +2,6 @@
 
 	$page['title'] = "Benvenuto";
 	$page['showTitle'] = false;
-	
+	$page['meta'] = array('keywords' => "key1 key2 key3",
+						  'contents' => "bla bla bla bla bla bla bla");	
 	return $page;
