fda85d31d53f667a91b3f1f57dfe33cbe5f23229
ilmich <ardutu@gmail.com>
2011-02-25 17:16:12 +0100

    Added simple sitemap generator(for now, work only with static pages)

---
+++ sitemap.php
@@ -0,0 +1,34 @@
+<?php
+
+	require_once "includes/master.inc.php";
+	
+	$res = new HttpResponse();
+	//array of site url
+	$urls = array();
+	
+	
+	//generate url for all pages
+	$pages = glob(getDataDir()."pages/*.metadata.php");	
+	foreach ($pages as $page) {
+		$id = basename($page,".metadata.php"); //recover id from filename
+		$stats = stat(getDataDir()."pages/".$id.".php"); //recover stat from file
+		$urls[$id]['loc'] = getWebRoot()."page.php?page=$id"; //generate url
+		$urls[$id]['lastmod'] = Date::dater($stats['mtime'],"Y-m-d"); //add last date mod
+	}
+	
+	//raise event in order to add url from external component
+	EventManager::getInstance()->getEvent("sitemapEvent")->raise($urls);
+	
+	$res->setMimeType("text/xml");	
+	$xml = "<?xml version='1.0' encoding='UTF-8'?>\n";
+	$xml .= "<urlset xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"
+			 xsi:schemaLocation=\"http://www.sitemaps.org/schema/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd\"
+			 xmlns=\"http://www.sitemaps.org/schemas/sitemap/0.9\">\n";
+	
+	foreach ($urls as $url) {
+		$xml .= "<url>\n\t<loc>".$url['loc']."</loc>\n\t<lastmod>".$url['lastmod']."</lastmod>\n</url>\n";		
+	}
+	
+	$xml .= "</urlset>";
+	$res->setBody($xml);
+	$res->send();
