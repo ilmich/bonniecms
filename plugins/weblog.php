80f4aa59133cd0ab9f49930953c19ecfbc5d973d
ilmich <ardutu@gmail.com>
2011-02-28 19:52:42 +0100

    Fix weblog plugin

--- plugins/weblog.php
+++ plugins/weblog.php
@@ -16,12 +16,16 @@
 		//lock file without polling or retry
 		//because weblogging is not a critical point of the application
 		//and skipping in case of lock failure is a reasonable thing		
-		if (File::lock($fileName,null,null)) {			
+		if (File::lock($fileName,null,1)) {			
 			$row = time()."|".
 					$req->getRemoteAddr()."|".$req->getUserAgent()."|".
 					$req->getMethod()."|".$req->getReferer()."|".Url::fullUrl()."\n";
 			file_put_contents($fileName,$row,FILE_APPEND);			
 			File::release($fileName);
+		}
+		else {
+			//but show a warn because failing to write weblogging slow down cms total speed
+			echo "<div style='color: #ff0000;'><b>WARN:Weblogging failed because the log dir is not writable. Fix this, or disable weblogging plugin!!!</b></div>";
 		}
 	}
 	
