b136f94c3eee3c1dd34116b75e7c555998286e22
ilmich <ardutu@gmail.com>
2011-02-28 16:29:07 +0100

    Removed obsolete getstarted page

--- data/pages/getstarted.metadata.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-	$page['title'] = "Get Started";
-	$page['showTitle'] = false;
-	$page['meta'] = array('keywords' => "key1 key2 key3",
-						  'contents' => "bla bla bla bla bla bla bla");		
-	return $page;
