a6bb1a5a72d5fc69730b3591fc76f8a0f3f0de29
ilmich <ardutu@gmail.com>
2011-03-12 23:59:38 +0100

    Better sitemap script

--- sitemap.php
+++ sitemap.php
@@ -2,33 +2,24 @@
 
 	require_once "includes/master.inc.php";
 	
-	$res = new HttpResponse();
+	$res = new HttpResponse("text/xml");
 	//array of site url
-	$urls = array();
-	
+	$urls = array();	
 	
 	//generate url for all pages
 	$pages = glob(getDataDir()."pages/*.metadata.php");	
 	foreach ($pages as $page) {
 		$id = basename($page,".metadata.php"); //recover id from filename
 		$stats = stat(getDataDir()."pages/".$id.".php"); //recover stat from file
-		$urls[$id]['loc'] = getWebRoot()."page.php?page=$id"; //generate url
-		$urls[$id]['lastmod'] = Date::dater($stats['mtime'],"Y-m-d"); //add last date mod
+		$urls['url'] = array('url' => array('loc' => getWebRoot()."page.php?page=$id",
+											'lastmod' => Date::dater($stats['mtime'],"Y-m-d"))
+							); //generate url		
 	}
 	
 	//raise event in order to add url from external component
 	EventManager::getInstance()->getEvent("sitemapEvent")->raise($urls);
 	
-	$res->setMimeType("text/xml");	
-	$xml = "<?xml version='1.0' encoding='UTF-8'?>\n";
-	$xml .= "<urlset xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"
-			 xsi:schemaLocation=\"http://www.sitemaps.org/schema/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd\"
-			 xmlns=\"http://www.sitemaps.org/schemas/sitemap/0.9\">\n";
+	$xml = ArrayUtils::toXml($urls,"urlset");
 	
-	foreach ($urls as $url) {		
-		$xml .= "<url>\n\t<loc>".Url::htmlEncode($url['loc'])."</loc>\n\t<lastmod>".$url['lastmod']."</lastmod>\n</url>\n";		
-	}
-	
-	$xml .= "</urlset>";
 	$res->setBody($xml);
 	$res->send();
