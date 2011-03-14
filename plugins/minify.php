b3b2c3e80e2cd445092224a0e7f7502d1ad8b8a6
ilmich <ardutu@gmail.com>
2011-03-15 00:04:14 +0100

    New Minify plugin

--- plugins/minify.php
+++ plugins/minify.php
@@ -1,12 +1,21 @@
 <?php if (!defined('CLYDEPHP')) die("Direct access not allowed") ;
 
 	require_once CLYDEPHP_VENDOR."minify/HTML.php";
-
-	EventManager::getInstance()->getEvent("processResponse")->subscribe("minifyResponse");
-	
-	function minifyResponse($req,$resp) {
+	require_once CLYDEPHP_VENDOR."minify/CSS.php";
 		
-		if ($resp->getMimeType() === 'text/html')
-			$resp->setBody(Minify_HTML::minify($resp->getBody()));		 
+	function minifyHtml($html=null) {
+		
+		if (!is_null($html) && is_string($html))
+			return Minify_HTML::minify($html);
+
+		return null;
         		
+	}
+	
+	function minifyCss($css,$options=array()) {
+		
+		if (!is_null($css) && is_string($css))
+			return Minify_CSS::minify($css,$options);
+
+		return null;		
 	}
