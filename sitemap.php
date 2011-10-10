6858b95edfd0f9cbb09f937d56d3783da71ee357
ilmich <ardutu@gmail.com>
2011-10-10 23:46:18 +0200

    Fixed sitemap generation

--- sitemap.php
+++ sitemap.php
@@ -8,18 +8,26 @@
 	
 	//generate url for all pages
 	$pages = glob(getDataDir().'pages/*.metadata.php');	
-	foreach ($pages as $page) {
+	foreach ($pages as $page) {                            
 		$id = basename($page,'.metadata.php'); //recover id from filename
+                if (strpos($id, '_') == 0) { continue; }
 		$stats = stat(getDataDir().'pages/'.$id.'.php'); //recover stat from file
-		$urls['url'] = array('url' => array('loc' => getWebRoot().'page.php?page='.$id,
-											'lastmod' => Date::dater($stats['mtime'],"Y-m-d"))
+		$urls[] = array('loc' => getWebRoot().'page.php?page='.$id,
+											'lastmod' => Date::dater($stats['mtime'],"Y-m-d")
 							); //generate url		
-	}
-	
+	}		
+        
 	//raise event in order to add url from external component
 	EventManager::getInstance()->getEvent('sitemapEvent')->raise($urls);
 	
-	$xml = ArrayUtils::toXml($urls,'urlset');
+        $xml = '<?xml version="1.0" encoding="utf-8"?>'.PHP_EOL.'<urlset>'.PHP_EOL;
+        foreach ($urls as $url) {
+            $xml .= '    <url>'.PHP_EOL;
+            $xml .= '        <loc>'.$url['loc'].'</loc>'.PHP_EOL;
+            $xml .= '        <lastmod>'.$url['lastmod'].'</lastmod>'.PHP_EOL;
+            $xml .= '    </url>'.PHP_EOL;
+        }
+        $xml .= '</urlset>';
 	
 	$res->setBody($xml);
 	$res->send();
