ad69703b4732097775792fc1365f5a34d8978fb7
ilmich <ardutu@gmail.com>
2012-02-14 01:11:14 +0100

    Automated web root configuration

--- configuration.inc.php
+++ configuration.inc.php
@@ -1,7 +1,8 @@
 <?php
 
 
-	$site['WEB_ROOT'] = "http://localhost/phpapps/bonniecms";
+	$site['WEB_ROOT'] = (isset($_SERVER['HTTPS'])) ? 	'https://'.$_SERVER['HTTP_HOST'].dirname($_SERVER['SCRIPT_NAME']) : 
+														'http://'.$_SERVER['HTTP_HOST'].dirname($_SERVER['SCRIPT_NAME']);
 	$site['SITE_NAME'] = "BonnieCMS SampleSite";
 	$site['SITE_SLOGAN'] = "...keep it simple...";	
 	$site['TEMPLATE'] = "winterplain";	
@@ -9,6 +10,6 @@
 	$site['DEFAULT_COMPONENT'] = "page";
 	$site['CACHE'] = true; //caching
 	$site['CACHE_TIME'] = 5; //cache timeout in seconds
-	$site['MINIFY'] = true;
+	$site['MINIFY'] = true;	
 	
 	return $site;
